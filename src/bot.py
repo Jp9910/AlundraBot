58744434146677b4868b162cded9906687538ff2
Joao Secundo <alundra@academico.ufs.br>
2022-07-19 22:40:58 -0300

    Message handling. Experimenting with background tasks (not working yet)

--- src/bot.py
+++ src/bot.py
@@ -1,22 +1,55 @@
 import discord
+from discord.ext import tasks
+from helper_functions import *
 import env
 
 class MyClient(discord.Client):
+
+    def __init__(self, *, loop=None, **options):
+        super().__init__(loop=loop, **options)
+        self.counter = 0
+
+    # Bot is ready
     async def on_ready(self):
         print('Logged on as', self.user)
 
+    # New message typed
     async def on_message(self, message):
-        # don't respond to ourselves
         print(f'Message from {message.author}: {message.content}')
 
-        if message.author == self.user:
+        if (message.author.bot):
             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        firstLetter = message.content[0]
 
+        match firstLetter:
+            case '!':
+                await handleCommand(message)
+            case default:
+                await handleMessage(message)
+                print(message.channel.id)
+
+    async def setup_hook(self) -> None:
+        # start the task to run in the background
+        self.my_background_task.start()
+
+    @tasks.loop(seconds=10)  # task runs every 10 seconds
+    async def my_background_task(self):
+        channel = self.get_channel(998764665364566038)  # channel ID goes here
+        self.counter += 1
+        await channel.send(self.counter)
+
+    @my_background_task.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()  # wait until the bot logs in
+
+        
 
 intents = discord.Intents.default()
-#intents.message_content = True
+
 client = MyClient(intents=intents)
 client.run(env.botkey)
+
+#print(vars(self.commandHandler))
+#print(self.commandHandler.__dict__)
+# from pprint import pprint; # pprint(self.commandHandler)
---
+++ src/helper_functions.py
@@ -0,0 +1,42 @@
+import asyncio
+import discord
+import os
+
+def messageFromBot(message: discord.Message) -> bool:
+    if (message.author.bot):
+        return True
+    return False
+
+async def handleCommand(message: discord.Message) -> None:
+    print("comando digitado: "+message.content[1:])
+    command = message.content[1:]
+    match command:
+        case 'alundra':
+            await replyWithMyImage(message)
+
+
+async def handleMessage(message: discord.Message) -> None:
+    match message.content:
+        case 'ping':
+            await message.channel.send('pong')
+        case 'vsf':
+            await message.reply('vá vc >:T')
+        case default:
+            await message.channel.send('123')
+
+async def replyWithMyImage(message: discord.Message) -> None:
+    # async with discord.channel.typing():
+    #     await asyncio.sleep(3)
+    print(os.listdir())
+    print(os.getcwd())
+    await message.channel.send(
+        content='Sou eu!',
+        file=discord.File('Resources/Images/alundra.png')
+    )
+    
+# Send file:    
+# with open('Resources/Images/alundra.png', 'rb') as fp:
+#     await message.channel.send(
+#         content='Sou eu!',
+#         file=discord.File(fp, 'newfilename.png')
+#     )
