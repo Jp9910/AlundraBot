21fbdf0eccd15eb3bd3321b0ae5e3b86293ff5a0
Joao Secundo <alundra@academico.ufs.br>
2022-07-24 01:25:38 -0300

    Update bot to work with cogs

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,7 @@
-from imaplib import Commands
 import discord
-from helper_functions import *
 from discord.ext import commands
+import asyncio
 import env
-import voice_bot
-import bot_tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,41 +11,15 @@ bot = commands.Bot(
     intents=intents
 )
 
-# Bot is ready
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
-    #await setup_hook(self)
+extensions = [
+    'bot_events',
+    'bot_tasks'
+]
 
-# New message typed
-@bot.event
-async def on_message(message: discord.Message):
-    print(f'Message from {message.author}: {message.content}')
+async def main():
+    async with bot:
+        for extension in extensions:
+            await bot.load_extension(extension)
+        await bot.start(env.botkey)
 
-    if (message.author.bot):
-        return
-
-    firstLetter = message.content[0]
-
-    match firstLetter:
-        case '!':
-            await handleCommand(message)
-        case default:
-            await handleMessage(message)
-            #print(message.channel.id)
-
-# Event in voice channel (Enter, leave, (un)mute, (un)deafen)
-@bot.event
-async def on_voice_state_update(member, voiceStateBefore, voiceStateAfter):
-    print('voice event')
-    channel = bot.get_channel(998764665364566038)
-    await channel.send('Voice event no canal ' + voiceStateAfter.channel.name)
-    await voiceStateAfter.channel.send('teste')
-
-#print(vars(self.commandHandler))
-#print(self.commandHandler.__dict__)
-# from pprint import pprint; # pprint(self.commandHandler)
-
-
-bot.run(env.botkey)
+asyncio.run(main())
