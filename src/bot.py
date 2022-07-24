363a6c92d2dd65c015cc01b29d4d2cf9bb65a458
Joao Secundo <alundra@academico.ufs.br>
2022-07-23 23:13:41 -0300

    Change bot to use discord.ext.commands instead of a discord.Client directly

--- src/bot.py
+++ src/bot.py
@@ -1,69 +1,54 @@
+from imaplib import Commands
 import discord
-from discord.ext import tasks
 from helper_functions import *
+from discord.ext import commands
 import env
-#import voice_bot_example
-
-class MyClient(discord.Client):
-
-    def __init__(self, *, loop=None, **options):
-        super().__init__(loop=loop, **options)
-        self.counter = 0
-
-    # Bot is ready
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-        #await self.setup_hook()
-
-    # New message typed
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
-        if (message.author.bot):
-            return
-
-        firstLetter = message.content[0]
-
-        match firstLetter:
-            case '!':
-                await handleCommand(message)
-            case default:
-                await handleMessage(message)
-                #print(message.channel.id)
-
-    # Event in voice channel (Enter, leave, (un)mute, (un)deafen)
-    async def on_voice_state_update(self, member, voiceStateBefore, voiceStateAfter):
-        print('voice event')
-        channel = self.get_channel(998764665364566038)
-        await channel.send('Voice event no canal ' + voiceStateAfter.channel.name)
-        await voiceStateAfter.channel.send('teste')
-
-
-    # A new setup_hook() method has also been added to the Client class. 
-    # This method is called after login but before connecting to the discord gateway.
-    async def setup_hook(self) -> None:
-        # start the task to run in the background
-        self.my_background_task.start()
-
-    @tasks.loop(seconds=2.0, count=1)  # task runs every 2.0 seconds, 3 times
-    async def my_background_task(self):
-        channel = self.get_channel(998764665364566038)  # channel ID goes here
-        self.counter += 1
-        await channel.send(self.counter)
-
-    @my_background_task.before_loop
-    async def before_my_task(self):
-        await self.wait_until_ready()  # wait until the bot logs in
-
-        
+import voice_bot
+import bot_tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(
+    command_prefix='!',
+    description="Alundra Bot",
+    intents=intents
+)
 
-client = MyClient(intents=intents)
-client.run(env.botkey)
+# Bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+    #await setup_hook(self)
+
+# New message typed
+@bot.event
+async def on_message(message: discord.Message):
+    print(f'Message from {message.author}: {message.content}')
+
+    if (message.author.bot):
+        return
+
+    firstLetter = message.content[0]
+
+    match firstLetter:
+        case '!':
+            await handleCommand(message)
+        case default:
+            await handleMessage(message)
+            #print(message.channel.id)
+
+# Event in voice channel (Enter, leave, (un)mute, (un)deafen)
+@bot.event
+async def on_voice_state_update(member, voiceStateBefore, voiceStateAfter):
+    print('voice event')
+    channel = bot.get_channel(998764665364566038)
+    await channel.send('Voice event no canal ' + voiceStateAfter.channel.name)
+    await voiceStateAfter.channel.send('teste')
 
 #print(vars(self.commandHandler))
 #print(self.commandHandler.__dict__)
 # from pprint import pprint; # pprint(self.commandHandler)
+
+
+bot.run(env.botkey)
