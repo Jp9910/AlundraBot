c1c6117f92a0f7de86849be9a31e094d65c0a9fa
Joao Secundo <alundra@academico.ufs.br>
2022-08-01 01:22:28 -0300

    Wavelink: 'move_to' and 'playing' commands

--- src/bot_wavelink.py
+++ src/bot_wavelink.py
@@ -1,6 +1,7 @@
 import wavelink
 from discord.ext import commands
 import async_spotify
+import discord
 
 # https://wavelink.readthedocs.io/en/latest/wavelink.html
 class BotWavelink(commands.Cog):
@@ -9,6 +10,8 @@ class BotWavelink(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.player: wavelink.Player = None
+        self.track: wavelink.Track = None
+        self.spotify_player: wavelink.Player = None
         bot.loop.create_task(self.connect_nodes())
 
     async def connect_nodes(self):
@@ -25,19 +28,33 @@ class BotWavelink(commands.Cog):
     async def on_wavelink_node_ready(self, node: wavelink.Node):
         """Event fired when a node has finished connecting."""
         print(f'Node: <{node.identifier}> is ready!')
+    
+    @commands.Cog.listener()
+    async def on_wavelink_websocket_closed(player: wavelink.Player, reason, code):
+        """Called when the Node websocket has been closed by Lavalink."""
+        print(f'Websocket closed. Reason: <{reason}>. Code: <{code}>')
 
     @commands.Cog.listener()
     async def on_wavelink_track_start(self, player: wavelink.Player, track: wavelink.Track):
         """Track started playing"""
+        self.track = track
         print(f'Playing: <{track.title}> - <{track.author}> (<{track.duration}>). stream? <{track.is_stream()}> ')
         # guild = ctx.guild
         # voice_client: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=guild)
         # await channel.send('Voice event no canal ' + voiceStateAfter.channel.name)
         # await voiceStateAfter.channel.send('Voice event aqui')
 
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Changes the player's volume"""
+        if self.player is None:
+            return await ctx.send("Not connected to a voice channel.")
+        await self.player.set_volume(volume)
+        await ctx.send(f"Changed volume to {volume}%/1000")
+
     @commands.command(aliases=['tocar'])
     async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
-        """Play a song with the given search query. If not connected, connect to our voice channel."""
+        """Play a song with the given search query. (Connects to your voice channel)"""
         if not ctx.voice_client:
             self.player: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
         else:
@@ -47,13 +64,13 @@ class BotWavelink(commands.Cog):
 
     @commands.command(aliases=['pare', 'pausar', 'parar'])
     async def pause(self, ctx):
-        """Pausa o que está sendo tocado"""
+        """Pauses song being played"""
         if self.player.is_playing():
             await self.player.pause()
 
     @commands.command(aliases=['continue', 'continuar'])
     async def resume(self, ctx):
-        """Despausa o que está sendo tocado"""
+        """Unpauses song being played"""
         if self.player.is_paused():
             await self.player.resume()
 
@@ -65,19 +82,32 @@ class BotWavelink(commands.Cog):
 
     @commands.command()
     async def stop(self, ctx):
-        """Para o que está sendo tocado"""
+        """Stops currently playing song"""
         if self.player.is_playing() or self.player.is_paused():
             await self.player.stop()
 
     @commands.command(aliases=['disconnect', 'leave', 'sai'])
     async def dc(self, ctx):
-        """Para e disconecta o bot do voice chat"""
+        """Stops and disconnects the bot from the voice chat"""
         await self.player.disconnect()
         self.player = None
+
+    @commands.command(aliases=['mover', 'venhapara', 'vapara', 'moveto'])
+    async def move_to(self, ctx, *, channel: discord.VoiceChannel = None):
+        """Move bot to another channel"""
+        if self.player:
+            await self.player.move_to(channel)
+
+    @commands.command(alises=['tocando'])
+    async def playing(self, ctx):
+        """Show currently playing song"""
+        if self.track:
+            await ctx.send(f'Playing: {self.track.title} - {self.track.author}. Duration: ({self.track.duration}) seconds. Source: <{self.track.uri}> Streaming? {self.track.is_stream()}')
 
     # https://pypi.org/project/async-spotify/
     @commands.command()
     async def spotify(self, ctx: commands.Context, url: str):
+        """Plays from spotify (wip)"""
         vc: wavelink.Player = (
             ctx.voice_client
             or await ctx.author.voice.channel.connect(cls=wavelink.Player)
